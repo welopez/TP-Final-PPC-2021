5835717f726f41a910780064bc5b00eca765b629
welopez <57297784+welopez@users.noreply.github.com>
2021-06-29 23:01:42 -0300

    Agregado persistir en memoria datos pantalla 4

--- app/src/main/java/com/example/ppc_tp3_isea/Screen4Activity.java
+++ app/src/main/java/com/example/ppc_tp3_isea/Screen4Activity.java
@@ -2,7 +2,9 @@ package com.example.ppc_tp3_isea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.view.View;
@@ -20,16 +22,33 @@ public class Screen4Activity extends AppCompatActivity {
     private RadioButton rad2;
     private Boolean selected_radiob;
 
+    Context context = this;
+    SharedPreferences sharedPrefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen4);
+
+        //Para guardar en memoria no volatil
+        sharedPrefs = getSharedPreferences("ArchivoSP", context.MODE_PRIVATE);
+
 
         ed1 = (EditText)findViewById(R.id.pacienteEditText);
         ed2 = (EditText)findViewById(R.id.riesgoEditText);
         ed3 = (EditText)findViewById(R.id.riesgoRecurrenteEditText);
         rad1 = (RadioButton)findViewById(R.id.radioButtonYes);
         rad2 = (RadioButton)findViewById(R.id.radioButtonNo);
+
+        ed1.setText(sharedPrefs.getString("Paciente", ""));
+        ed2.setText(sharedPrefs.getString("RiesgoProg", ""));
+        ed3.setText(sharedPrefs.getString("RiesgoRec", ""));
+        if(sharedPrefs.getBoolean("Esquema", false) == true){
+            rad1.setChecked(true);
+        }else{
+            rad2.setChecked(true);
+        }
+
 
         InputFilter limitFilter = new MinMaxInputFilter(1, 10);
         ed2.setFilters(new InputFilter[] { limitFilter });
@@ -45,10 +64,23 @@ public class Screen4Activity extends AppCompatActivity {
             selected_radiob = false;
         }
 
-        Bundle extras = new Bundle(); // Pasamos los datos necesarios para la siguiente activity.
-        extras.putDouble("riesgoProg", Double.parseDouble(ed2.getText().toString()));
-        extras.putDouble("riesgoRec", Double.parseDouble(ed3.getText().toString()));
-        extras.putBoolean("esquema", rad1.isChecked());
+        Double riesgoProg = Double.parseDouble(ed2.getText().toString());
+        Double riesgoRec = Double.parseDouble(ed3.getText().toString());
+        Boolean esquema = rad1.isChecked();
+
+        //Guardamos datos en memoria no volatil
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString("Paciente", ed1.getText().toString());
+        editor.putString("RiesgoProg", ed2.getText().toString());
+        editor.putString("RiesgoRec", ed3.getText().toString());
+        editor.putBoolean("Esquema", esquema);
+        editor.commit();
+
+        // Pasamos los datos necesarios para la siguiente activity.
+        Bundle extras = new Bundle();
+        extras.putDouble("riesgoProg", riesgoProg);
+        extras.putDouble("riesgoRec", riesgoRec);
+        extras.putBoolean("esquema", esquema);
 
         Intent goact5 = new Intent(this, Screen5Activity.class);
         goact5.putExtras(extras);
